95b60e6ab9f36657dddbdc848f3ae0ad7c749aa6
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-27 05:09:15 -0400

    Use strict SciMLTesting 2.4 QA
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -1,13 +1,4 @@
 using SciMLTesting, SparseMatrixIdentification
 using JET
 
-run_qa(
-    SparseMatrixIdentification;
-    explicit_imports = true,
-    ei_kwargs = (;
-        # `fast_scalar_indexing` is not public (unexported, not declared public) in
-        # ArrayInterface; it is the only non-public name this package accesses.
-        all_qualified_accesses_are_public = (; ignore = (:fast_scalar_indexing,)),
-    ),
-    api_docs_kwargs = (; rendered = true),
-)
+run_qa(SparseMatrixIdentification)
